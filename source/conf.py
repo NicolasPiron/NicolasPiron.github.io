c2c0411d9cec1a7153be8039e74c146ffc6a817d
Nicolas Piron <ncls.piron@gmail.com>
2026-02-09 14:04:14 +0100

    first test

--- source/conf.py
+++ source/conf.py
@@ -7,8 +7,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Kim Kitsuragi'
-author = 'Victor Ferat'
-copyright = '2026, Victor Ferat'
+author = 'Nicolas Piron'
+copyright = '2026, Nicolas Piron'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
